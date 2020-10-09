834d00d0ab74503c530e01b4446612c789cd2a18
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-10-09 15:28:23 -0700

    Added some breakpoints to css and solved 1 stretch js

--- index.js
+++ index.js
@@ -337,25 +337,25 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */){
+function getHTML(arr){
 
-    /* Code here */
-
-  }
+}
 
 
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
+function randomize(arr){
+const newarr = [];
+for(let i = 0; i < arr.length; i++){
+newarr.push(arr[Math.floor(Math.random()*arr.length)]);
+}
+return newarr;
   }
-
+console.log(randomize(artists));
 
  /* 💪💪💪💪💪💪 STRETCH 3: 💪💪💪💪💪💪
- Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
+ Use advanced array methods (.map, .reduce, .filter) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
 
  
  
