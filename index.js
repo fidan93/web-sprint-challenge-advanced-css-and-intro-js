fb759bdf89f95f3920507a551cd69287b8324f28
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-10-09 09:09:20 -0700

    completed index.js MVP tasks

--- index.js
+++ index.js
@@ -209,12 +209,16 @@ Practice accessing data above by console.log-ing following items:
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+ console.log(artists[0].name);
+ console.log(artists[2].bio);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
-
+if(artists[8].name !== 'Vincent Van Gogh'){
+  artists[8].name = 'Vincent Van Gogh';
+}
+console.log(artists[8].name);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called `getArtistByIndex` that takes two arguments:
@@ -225,19 +229,29 @@ Create a function called `getArtistByIndex` that takes two arguments:
  * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(arr,id) {
+  return `the artist at index ${id} is ${arr[id].name}`
 }
 
-  
+  console.log(getArtistByIndex(artists,8));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Create a function called get20s() that takes data as an argument and returns an array with names of artists who were born in and died in 20th century (1900-2000) example born in 1901 and died in 1959 - included / born in 1889 and died in 1925 not included - should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(arr){
+  const newarr = [];
+  for(let i = 0; i < arr.length; i++){
+    let years = arr[i].years.split(' - ');
+    // console.log(arr);
+    if(years[0] > 1900 && years[1]<2000){
+      newarr.push(arr[i].name);
+      
+    }
+  }
+   return newarr;
+  
 }
-
+console.log(get20s(artists))
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -251,10 +265,12 @@ Create a function called `removeArtist` that takes two arguments:
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
 */
-function removeArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function removeArtist(arr,id) {
+ arr.splice(id,1);
+ console.log(arr)
+ return arr.length;
 }
-   
+  console.log(removeArtist(artists,1)); 
 
 /**
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -270,10 +286,11 @@ bio: Add 1-2 sentences (or use lorem ipsum)
 
 At the end, this function should return the new array with information added"*/
 
-function addArtist(/*Your Code Here*/){
-  /*Your Code Here*/
+function addArtist(arr,name,years,genre,nationality,bio){
+   arr.push({name:name,years:years,genre:genre,nationality:nationality,bio:bio});
+   return arr;
   }
-
+console.log(addArtist(artists,'Abidin Dino','1913 - 1993','Abstract expressionism','Turkish','The art world is known to feature great artists from different walks of life, there have been very few Ottoman artists who managed to make an impact and Abidin Dino was one of them. At the ripe age of 20 Dino founded the D Group of artists and six years later he founded the Liman Group. '))
   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -285,11 +302,17 @@ and returns an array with names of artists who painted more than 100 paintings.
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ..."Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(arr){
+  const newarr = [];
+  for(let i = 0; i < arr.length; i++){
+  if(arr[i].paintings > 100){
+   newarr.push(arr[i].name);
+  }
+  }
+  return newarr;
 }
 
-
+console.log(lotsOfArt(artists));
 
 
 // 🎨🎨 STRETCH 🎨🎨//
